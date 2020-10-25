6decfb715abe42ebcd2f640e3a163afd71ea715b
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2020-10-25 14:48:03 +0100

    Add files via upload

---
+++ bmi/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#include <iomanip>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+    int masa;
+
+    double wzrost;
+    void obliczBMI(int, double);
+
+
+    cout<<"Masa"<<  endl;
+    cout<<"Wzrost"<< endl;
+    cin >> masa >> wzrost;
+
+    obliczBMI(masa, wzrost);
+
+
+
+    return 0;
+
+}
+
+
+
+void obliczBMI(int m, double w) {
+
+    double BMI = m / (w * w);
+
+
+
+    cout << setprecision(6) << BMI << endl;
+
+
+
+    if(BMI > 30)
+
+        cout << "OTYLOSC";
+
+    else if (BMI <= 30 && BMI > 25)
+
+        cout << "NADWAGA";
+
+    else if (BMI <= 25 && BMI >= 20)
+
+        cout << "NORMA";
+
+    else
+
+        cout << "NIEDOWAGA";
+
+}
